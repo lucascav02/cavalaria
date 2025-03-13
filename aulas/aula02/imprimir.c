6e9f940d44f0ba479bfb5dbfbe76cf842e35184c
lucasbrito <nascimentobritolucas6@gmail.com>
2025-03-12 21:53:21 -0300

    codigos da aula 2

---
+++ aulas/aula02/imprimir.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Boa noite\n\n");
+    printf("\tEstamos aprendendo C!");
+    return 0;
+
+}
+
+
---
+++ aulas/aula02/imprimir_caracter.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("%c\n", 'A');
+    printf("ola %s\n", "jose");
+    return 0;
+}
